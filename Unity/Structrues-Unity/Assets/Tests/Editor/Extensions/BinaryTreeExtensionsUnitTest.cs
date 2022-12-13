5c148b149e56ec8d6b1f62305afbfbf708bc20df
Leon Malakov <leon-malakov@mail.ru>
2022-12-13 17:25:46 +0300

    Add unity version

---
+++ Unity/Structrues-Unity/Assets/Tests/Editor/Extensions/BinaryTreeExtensionsUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NuclearGames.StructuresUnity.Extensions;
+using NuclearGames.StructuresUnity.Randoms;
+using NuclearGames.StructuresUnity.Structures.BST;
+using NUnit.Framework;
+
+namespace Tests.Editor.Extensions {
+    internal class BinaryTreeExtensionsUnitTest {
+        [Test]
+        public void RandomTest() {
+            var tree = new BinaryTree<int>();
+            var random = new UniformRandom();
+
+            for (int i = 0; i < 1000; i++) {
+                int sign = random.Next(0, 1, false) == 1 ? -1 : 1;
+                tree.Add(i * sign);
+            }
+
+            for (int i = 0; i < 100; i++) {
+                var randomElement = tree.GetRandom(random);
+                Assert.IsTrue(tree.Contains(randomElement));
+            }
+        }
+    }
+}
---
+++ Unity/Structrues-Unity/Packages/Structures-Unity/Runtime/Extensions/BinaryTreeExtensions.cs
@@ -0,0 +1,48 @@
+﻿using NuclearGames.StructuresUnity.Randoms.Interfaces;
+using NuclearGames.StructuresUnity.Structures.BST;
+using NuclearGames.StructuresUnity.Structures.BST.Utils;
+using System;
+
+namespace NuclearGames.StructuresUnity.Extensions {
+    public static class BinaryTreeExtensions {
+        /// <summary>
+        /// Возвращает случайный элемент дерева.
+        /// <para>
+        /// Использует рандомайзер для получаения случайного индекса,
+        /// после чего итерируется по дереву пока не достигнет целевого индекса.
+        /// </para>
+        /// </summary>
+        public static TData GetRandom<TData, TComparable>(this BaseBinaryTree<TData, TComparable> tree, IRandomizer randomizer)
+            where TComparable : IComparable<TComparable> {
+
+            int randomIndex = randomizer.Next(0, tree.Count);
+            int currentIndex = -1;
+            Node<TData> result = null;
+
+            bool Iteration(Node<TData> node) {
+                currentIndex++;
+
+                if (currentIndex == randomIndex) {
+                    result = node;
+                    return true;
+                }
+
+                if (node.Left != null && Iteration(node.Left)) {
+                    return true;
+                }
+
+                if (node.Right != null && Iteration(node.Right)) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (!Iteration(tree.Root!)) {
+                throw new Exception();
+            }
+
+            return result!.Data;
+        }
+    }
+}
