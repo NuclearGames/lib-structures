0bc767003e1a666093c7e2459f4f1ea2f6ec354f
Leon Malakov <leon-malakov@mail.ru>
2022-12-13 17:10:18 +0300

    Add binary tree random extension

---
+++ Structures-NetSixZero/Extensions/BinaryTreeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Structures.NetSixZero.Randoms.Interfaces;
+using Structures.NetSixZero.Structures.BST;
+using Structures.NetSixZero.Structures.BST.Utils;
+
+namespace Structures.NetSixZero.Extensions {
+    public static class BinaryTreeExtensions {
+        /// <summary>
+        /// Возвращает случайный элемент дерева.
+        /// <para>
+        /// Использует рандомайзер для получаения случайного индекса,
+        /// после чего итерируется по дереву пока не достигнет целевого индекса.
+        /// </para>
+        /// </summary>
+        public static TData GetRandom<TData, TComparable>(this BaseBinaryTree<TData, TComparable> tree, IRandomizer randomizer) 
+            where TComparable : IComparable<TComparable> {
+
+            int randomIndex = randomizer.Next(0, tree.Count);
+            int currentIndex = -1;
+            Node<TData>? result = null;
+
+            bool Iteration(Node<TData> node) {
+                currentIndex++;
+
+                if (currentIndex == randomIndex) {
+                    result = node;
+                    return true;
+                }
+
+                if(node.Left != null && Iteration(node.Left)) {
+                    return true;
+                }
+
+                if (node.Right != null && Iteration(node.Right)) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (!Iteration(tree.Root!)) {
+                throw new Exception();
+            }
+
+            return result!.Data;
+        }
+    }
+}
---
+++ Structures-UnitTests-NetSixZero/Extensions/BinaryTreeExtensionsUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Structures.NetSixZero.Extensions;
+using Structures.NetSixZero.Randoms;
+using Structures.NetSixZero.Structures.BST;
+
+namespace Structures_UnitTests_NetSixZero.Extensions {
+    internal class BinaryTreeExtensionsUnitTest {
+        [Test]
+        public void RandomTest() {
+            var tree = new BinaryTree<int>();
+            var random = new UniformRandom();
+
+            for (int i = 0; i < 1000; i++) {
+                int sign = random.Next(0, 1, false) == 1 ? -1 : 1;
+                tree.Add(i * sign);
+            }
+
+            for (int i = 0; i < 100; i++) {
+                var randomElement = tree.GetRandom(random);
+                Assert.IsTrue(tree.Contains(randomElement));
+            }
+        }
+    }
+}
