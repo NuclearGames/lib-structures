2126d73495543c4fc819997dffe32fe815412cd9
Leon Malakov <leon-malakov@mail.ru>
2022-12-20 12:07:27 +0300

    Add ReadOnlyDictionaryAdapter

---
+++ Structures-NetSixZero/Utils/Collections/ReadOnlyDictionaryAdapter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+namespace Structures.NetSixZero.Utils.Collections {
+    /// <summary>
+    /// Адаптер для IReadOnlyDictionary словаря. Преобразует тип <typeparamref name="TRawValue"/> в его базовый <typeparamref name="TValue"/>.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TRawValue"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class ReadOnlyDictionaryAdapter<TKey, TValue, TRawValue> : IReadOnlyDictionary<TKey, TValue> where TRawValue : TValue {
+        public TValue this[TKey key] => _source[key];
+        public IEnumerable<TKey> Keys => _source.Keys;
+        public IEnumerable<TValue> Values => _source.Values.Cast<TValue>();
+        public int Count => _source.Count;
+
+        private readonly IReadOnlyDictionary<TKey, TRawValue> _source;
+
+        public ReadOnlyDictionaryAdapter(IReadOnlyDictionary<TKey, TRawValue> source) {
+            _source = source;
+        }
+
+        public bool ContainsKey(TKey key) {
+            return _source.ContainsKey(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value) {
+            if (_source.TryGetValue(key, out var rawValue)) {
+                value = rawValue;
+                return true;
+            }
+            value = default!;
+            return false;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+            return new Enumerator(_source);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return new Enumerator(_source);
+        }
+
+        public struct Enumerator : IEnumerator<KeyValuePair<TKey, TValue>>, IEnumerator, IDisposable {
+            private readonly IEnumerator<KeyValuePair<TKey, TRawValue>> _source;
+
+            public Enumerator(IReadOnlyDictionary<TKey, TRawValue> source) : this() {
+                _source = source.GetEnumerator();
+            }
+
+            public KeyValuePair<TKey, TValue> Current => new KeyValuePair<TKey, TValue>(_source.Current.Key, _source.Current.Value);
+            object IEnumerator.Current => _source.Current;
+
+            public bool MoveNext() {
+                return _source.MoveNext();
+            }
+
+            public void Dispose() {
+                _source.Dispose();
+            }
+
+            public void Reset() {
+                _source.Reset();
+            }
+        }
+    }
+}
---
+++ Structures-UnitTests-NetSixZero/Utils/Collections/ReadOnlyDictionaryAdapterUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Structures.NetSixZero.Utils.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Structures_UnitTests_NetSixZero.Utils.Collections {
+    internal class ReadOnlyDictionaryAdapterUnitTests {
+        private interface ISomeType {
+            int Value { get; set; }
+        }
+
+        private class SomeType : ISomeType {
+            public int Value { get; set; }
+            public SomeType(int value = 0) {
+                Value = value;
+            }
+        }
+
+        [Test]
+        public void Main() {
+            // Проверяет:
+            // - Данные возвращаемые адаптером точно соответсвуют данным источника.
+
+            var source = new Dictionary<string, SomeType>();
+            IReadOnlyDictionary<string, ISomeType> adapter = new ReadOnlyDictionaryAdapter<string, ISomeType, SomeType>(source);
+
+            // Сравнивает souce и adapter.
+            void AssertState() {
+                Assert.AreEqual(adapter.Count, source.Count);
+                Assert.IsTrue(Enumerable.SequenceEqual(adapter.Keys, source.Keys));
+                Assert.IsTrue(Enumerable.SequenceEqual(adapter.Values, source.Values));
+
+                foreach (var key in source.Keys) {
+                    Assert.AreEqual(adapter.ContainsKey(key), source.ContainsKey(key));
+                    Assert.AreEqual(adapter[key], source[key]);
+                    Assert.IsTrue(
+                        adapter.TryGetValue(key, out var adapterValue) == source.TryGetValue(key, out var sourceValue) &&
+                        adapterValue == sourceValue);
+                }
+
+                var adapterEnumeration = adapter.ToArray();
+                var sourceEnumeration = source.ToArray();
+                Assert.IsTrue(Enumerable.SequenceEqual(adapterEnumeration.Select(x => x.Key), sourceEnumeration.Select(x => x.Key)));
+                Assert.IsTrue(Enumerable.SequenceEqual(adapterEnumeration.Select(x => x.Value), sourceEnumeration.Select(x => x.Value)));
+            }
+
+            // Run.
+            AssertState();
+            source.Add("abc", new SomeType(5));
+            source.Add("bbb", new SomeType(2));
+            AssertState();
+            source.Remove("abc");
+            AssertState();
+            source.Add("qwe", new SomeType(2));
+            source.Add("rey", new SomeType(2));
+            AssertState();
+            source.Clear();
+            AssertState();
+        }
+    }
+}
