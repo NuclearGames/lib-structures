32b5e864d11709c372ad238f42f097f339a1d467
Leon Malakov <leon-malakov@mail.ru>
2022-12-20 16:36:11 +0300

    Add BaseIncrementIdGenerator

---
+++ Structures-UnitTests-NetSixZero/Utils/Generators/BaseIncrementIdGeneratorUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Structures.NetSixZero.Utils.Generators;
+using System.Collections.Generic;
+
+namespace Structures_UnitTests_NetSixZero.Utils.Generators {
+    internal class BaseIncrementIdGeneratorUnitTests {
+        private class TestIncrementGenerator : BaseIncrementIdGenerator<int> {
+            internal TestIncrementGenerator(int initial) : base(initial) { }
+            protected override int Increment(int x) {
+                unchecked {
+                    return x + 1;
+                }
+            }
+        }
+
+        [Test]
+        public void GetNextTest() {
+            var generator = new TestIncrementGenerator(0);
+
+            var ids = new HashSet<int>();
+            for(int i = 0; i < 100; i++) {
+                Assert.That(ids.Add(generator.GetNext()));
+            }
+
+            generator = new TestIncrementGenerator(int.MaxValue - 1);
+            ids.Clear();
+
+            Assert.That(generator.GetNext(), Is.EqualTo(int.MaxValue - 1));
+            Assert.That(generator.GetNext(), Is.EqualTo(int.MaxValue));
+            Assert.That(generator.GetNext(), Is.EqualTo(int.MinValue));
+            Assert.That(generator.GetNext(), Is.EqualTo(int.MinValue + 1));
+        }
+    }
+}
---
+++ Unity/Structrues-Unity/Packages/Structures-Unity/Runtime/Utils/Generators/BaseIncrementIdGenerator.cs
@@ -0,0 +1,31 @@
+﻿namespace NuclearGames.StructuresUnity.Utils.Generators {
+    /// <summary>
+    /// Базовый тип циклического генератора, инкрементирующего значение.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseIncrementIdGenerator<T> where T : struct {
+        private readonly object _locker;
+        private T _next;
+
+        protected BaseIncrementIdGenerator(T initial) {
+            _locker = new object();
+            _next = initial;
+        }
+
+        /// <summary>
+        /// Возвращает следующее значение.
+        /// </summary>
+        public T GetNext() {
+            lock (_locker) {
+                T current = _next;
+                _next = Increment(_next);
+                return current;
+            }
+        }
+
+        /// <summary>
+        /// Должен увеличить значение циклически.
+        /// </summary>
+        protected abstract T Increment(T x);
+    }
+}
